c9037ef94bff7a4fb7a2d77a1e3976a49df30887
buchtaro <robin.buchta@stud.hs-hannover.de>
2020-04-09 15:09:44 +0200

    donnerstagSession vorbei

--- FirstRun.py
+++ FirstRun.py
@@ -2,17 +2,35 @@
 """
 Created on Thu Apr  2 13:54:13 2020
 
-@author: robi & pili
+@author: robi & phili
 """
 
 
 import pandas as pd
 from mlxtend.preprocessing import TransactionEncoder
-from mlxtend.frequent_patterns import association_rules
+#from mlxtend.frequent_patterns import association_rules
 from mlxtend.frequent_patterns import apriori
+from mlxtend.frequent_patterns import fpgrowth
 import numpy as np
 import csv
 import re
+import association_rules as own
+import dataframePrint as ownPrint
+
+
+#pd.set_option('display.max_rows', 50000)
+#pd.set_option('display.width', 10000)
+#pd.set_option('display.max_columns', None)
+#pd.set_option('display.max_colwidth', None)
+
+#Variables
+show_rules = 2
+kluc_threshold = 0.8
+kluc_range_max = 0.6
+kluc_range_min = 0.5
+imb_ratio_threshold = 0.5
+association_rules_threshold = 0.4
+min_sup_threshold = 0.0003
 
 print("Start")
 df = pd.read_csv("crimes_e_columns_ficken.csv", low_memory=False)
@@ -34,22 +52,35 @@ te = TransactionEncoder()
 te_ary = te.fit(df2).transform(df2)
 df1 = pd.DataFrame(te_ary, columns=te.columns_)
 
-frequent_itemsets = apriori(df1, min_support=0.0001, use_colnames=True, low_memory=True)
+#frequent_itemsets = apriori(df1, min_support=0.0001, use_colnames=True, low_memory=True)
+frequent_itemsets = fpgrowth(df1, min_support=min_sup_threshold, use_colnames=True)
 print("Frequent_Itemsets created")
-#print(frequent_itemsets)
+print(len(frequent_itemsets))
 
-result = association_rules(frequent_itemsets, min_threshold=0.2)
-#print(result['lift'])
-#result = association_rules(frequent_itemsets, min_threshold=0.5)6g vbrdf e
+result = own.association_rules(frequent_itemsets, min_threshold=association_rules_threshold)
+print("Association Rules created")
 
-print("Association Rules")
-#print(result)
-result.to_csv('out.csv', index=False) 
+result.to_csv('out.csv', index=False)
+print("Association Rules saved as csv") 
+
+print("Filter Rules")
 df2 = result
 #Füge eine neue Spalte mit der Länge dem Dataframe hinzu
+print("Interesiting Kluc created")
 df2["antecedent_len"] = df2["antecedents"].apply(lambda x: len(x))
-print(df2[ (df2['antecedent_len'] >= 3) ])
+kluc = df2[ (df2['antecedent_len'] >= show_rules) &
+            (df2['kluc'] >= kluc_threshold )]
+kluc.to_csv('kluc.csv', index=False)
+#print(kluc)
+ownPrint.print_full(kluc)
 #hier ist ein Dataframe, welches frozensets enthält -> deswegen inhalt zu string convertieren und dann contains prüfen können
 #print(df2[df2['antecedents'].astype(str).str.contains(',')]) 
 #print(df2)
+print("interisting imb_ratio created")
+imb = df2[ (df2['antecedent_len'] >= show_rules)  & 
+            ( (df2['kluc'] < kluc_range_max) & (df2['kluc'] >= kluc_range_min) )  & 
+            (df2['imb ratio'] >= imb_ratio_threshold)]
+imb.to_csv('imb.csv', index=False)
+#print(imb)
+#ownPrint.print_full(imb)
 print("Finished")
---
+++ dataframePrint.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  9 12:25:06 2020
+
+@author: robi & phili
+"""
+import pandas as pd
+
+def print_full(x):
+    pd.set_option('display.max_rows', len(x))
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', 2000)
+    pd.set_option('display.float_format', '{:20,.10f}'.format)
+    pd.set_option('display.max_colwidth', -1)
+    print(x)
+    pd.reset_option('display.max_rows')
+    pd.reset_option('display.max_columns')
+    pd.reset_option('display.width')
+    pd.reset_option('display.float_format')
+    pd.reset_option('display.max_colwidth')
