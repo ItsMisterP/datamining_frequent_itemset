af7790398d6e994bb5c7fe8aec8426a367cb6c81
RobinBuchta <buchta.robin@outlook.de>
2020-04-18 23:49:06 +0200

    Push

--- statistics_bigdata.py
+++ statistics_bigdata.py
@@ -30,7 +30,7 @@ def statisticsPrint(dataframe):
     print(dataframe.value_counts())
     print()
     #print(dataframe.value_counts().to_json())
-    dataframe.value_counts().to_json(r'Counts_json\DataframeValueCounts' + dataframe[0] + '.json')
+    #dataframe.value_counts().to_json(r'Counts_json\DataframeValueCounts' + dataframe[0] + '.json')
 
 print("Hier sind alle Spalten:")
 print(df.columns)
@@ -43,28 +43,36 @@ print()
 print("--------------PRIMARY DESCRIPTION:---------------")
 primary_description = df['time']
 statisticsPrint(primary_description)
+primary_description.value_counts().to_json(r'Counts_json\DataframeValueCounts' + 'TIME'+ '.json')
 print("--------------SECONDARY DESCRIPTION:---------------")
 secondary_description = df['Block']
 statisticsPrint(secondary_description)
+secondary_description.value_counts().to_json(r'Counts_json\DataframeValueCounts' + 'BLOCK' + '.json')
 print("--------------BLOCK:---------------")
 block = df['Primary Type']
 statisticsPrint(block)
+block.value_counts().to_json(r'Counts_json\DataframeValueCounts' + 'PRIMARYTYPE' + '.json')
 print("--------------LOCATION DESCRIPTION:---------------")
 location_description = df['Description']
 statisticsPrint(location_description)
+location_description.value_counts().to_json(r'Counts_json\DataframeValueCounts' + 'DESCRIPTION' + '.json')
 print("--------------DAY---------------")
 day = df['Location Description']
 statisticsPrint(day)
+day.value_counts().to_json(r'Counts_json\DataframeValueCounts' + 'LOCATIONDESCRIPTION' + '.json')
 print("--------------AM/PM---------------")
 ampm = df['year']
 statisticsPrint(ampm)
+ampm.value_counts().to_json(r'Counts_json\DataframeValueCounts' + 'YEAR' + '.json')
 print("--------------TIME---------------")
 time = df['month']
 statisticsPrint(time)
+time.value_counts().to_json(r'Counts_json\DataframeValueCounts' + 'MONTH' + '.json')
 print("--------------TIME---------------")
-time = df['weekday']
+time2 = df['weekday']
 statisticsPrint(time)
+time2.value_counts().to_json(r'Counts_json\DataframeValueCounts' + 'WEEKDAY' + '.json')
 print("--------------TIME---------------")
-time = df['t']
+time3 = df['t']
 statisticsPrint(time)
- 
+time3.value_counts().to_json(r'Counts_json\DataframeValueCounts' + 'T' + '.json')
