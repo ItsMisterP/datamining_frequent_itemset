27ef8e214a221afa825e915022da637885c31dc7
Jannik Badenhop <jannik.badenhop@web.de>
2020-05-22 10:35:28 +0200

    .gitignore update

--- website/src/pages/Dashboard.vue
+++ website/src/pages/Dashboard.vue
@@ -3,203 +3,39 @@
         <div class="md-layout">
             <div
                 class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-50"
+                layout="row"
             >
                 <md-card>
                     <md-card-header data-background-color="gray">
-                        <h4 class="title">Verbrechen nach den Wochentagen</h4>
+                        <h4 class="title">Crime by District Heatmap</h4>
+                        <p class="category">
+                            Total crime count per district.
+                        </p>
+                    </md-card-header>
+                    <md-card-content>
+                        <heatmap
+                            id="1"
+                            :districtCrimeCount="loadedData['District']"
+                            @update-selectedDistrict="changeSelectedDistrict"
+                        ></heatmap>
+                    </md-card-content>
+                </md-card>
+                <md-card>
+                    <md-card-header data-background-color="gray">
+                        <h4 class="title">Crime by District Heatmap</h4>
                         <p class="category">
                             Zeigt die absolute Anzahl nach Wochentag an.
                         </p>
                     </md-card-header>
                     <md-card-content>
-                        <chart
-                            :data='loadedData["weekday"]'
-                            id="9"
-                        ></chart>
-                    </md-card-content>
-                </md-card>
-            </div>
-
-            <div
-                class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-50"
-            >
-                <md-card>
-                    <md-card-header data-background-color="gray">
-                        <h4 class="title">Vebrechen nach der Uhrzeit</h4>
-                        <p class="category">
-                            Zeigt die absolute Anzahl nach der Uhrzeit an.
-                        </p>
-                    </md-card-header>
-                    <md-card-content>
-                        <column
-                            :data='loadedData["time"]'
-                            id="8"
-                        ></column>
-                    </md-card-content>
-                </md-card>
-            </div>
-
-            <div
-                class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-50"
-            >
-                <md-card>
-                    <md-card-header data-background-color="gray">
-                        <h4 class="title">Verbrechen pro Block (Top 30)</h4>
-                        <p class="category">
-                            Zeigt die absolute Anzahl pro Block an.
-                        </p>
-                    </md-card-header>
-                    <md-card-content>
-                        <column
-                            :data='loadedData["Block"]'
-                            id="7"
-                        ></column>
-                    </md-card-content>
-                </md-card>
-            </div>
-
-            <div
-                class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-50"
-            >
-                <md-card>
-                    <md-card-header data-background-color="gray">
-                        <h4 class="title">Verbechen pro Location (Top 30)</h4>
-                        <p class="category">
-                            Zeigt die absolute Anzahl nach der Location an.
-                        </p>
-                    </md-card-header>
-                    <md-card-content>
-                        <column
-                            :data='loadedData["LocationDescription"]'
-                            id="6"
-                        ></column>
-                    </md-card-content>
-                </md-card>
-            </div>
-
-            <div
-                class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-50"
-            >
-                <md-card>
-                    <md-card-header data-background-color="gray">
-                        <h4 class="title">
-                            Verbrechen pro "Primary Description"
-                        </h4>
-                        <p class="category">
-                            Zeigt die absolute Anzahl der "Primary Description"
-                            an.
-                        </p>
-                    </md-card-header>
-                    <md-card-content>
-                        <column
-                            :data='loadedData["PrimaryType"]'
-                            id="5"
-                        ></column>
-                    </md-card-content>
-                </md-card>
-            </div>
-
-            <div
-                class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-50"
-            >
-                <md-card>
-                    <md-card-header data-background-color="gray">
-                        <h4 class="title">
-                            Verbrechen pro "Secondary Description"
-                        </h4>
-                        <p class="category">
-                            Zeigt die absolute Anzahl der "Secondary
-                            Description" an.
-                        </p>
-                    </md-card-header>
-                    <md-card-content>
-                        <column
-                            :data='loadedData["Description"]'
-                            id="3"
-                        ></column>
-                    </md-card-content>
-                </md-card>
-            </div>
-
-            <div
-                class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-50"
-            >
-                <md-card>
-                    <md-card-header data-background-color="gray">
-                        <h4 class="title">
-                            Verbrechen je nach Tageszeit (grob)
-                        </h4>
-                        <p class="category">
-                            Zeigt die absolute Anzahl nach AM und PM an.
-                        </p>
-                    </md-card-header>
-                    <md-card-content>
-                        <chart
-                            :data='loadedData["t"]'
-                            id="1"
-                        ></chart>
-                    </md-card-content>
-                </md-card>
-            </div>
-
-            <div
-                class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-50"
-            >
-                <md-card>
-                    <md-card-header data-background-color="gray">
-                        <h4 class="title">Vebrechen pro Monat</h4>
-                        <p class="category">
-                            Zeigt die absolute Anzahl der Verbrachen pro Monat
-                            an.
-                        </p>
-                    </md-card-header>
-                    <md-card-content>
-                        <column
-                            :data='loadedData["month"]'
+                        <piechart
                             id="2"
-                        ></column>
+                            :selectedDistrict="this.selectedDistrict"
+                            @update-selectedDistrict="changeSelectedDistrict"
+                        ></piechart>
                     </md-card-content>
                 </md-card>
             </div>
-
-            <div
-                class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-50"
-            >
-                <md-card>
-                    <md-card-header data-background-color="gray">
-                        <h4 class="title">
-                            Zeitliche Entwicklung der Verbrechen
-                        </h4>
-                        <p class="category">
-                            Zeigt die zeitliche Entwicklung der absoluten Anzahl
-                            der Verbrachen.
-                        </p>
-                    </md-card-header>
-                    <md-card-content>
-                        <linechart
-                            :data='loadedData["year"]'
-                            id="4"
-                        ></linechart>
-                    </md-card-content>
-                </md-card>
-            </div>
-            <!--
-            <div
-                class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-50"
-            >
-                <md-card>
-                    <md-card-header data-background-color="gray">
-                        <h4 class="title">
-                            Items und ihre relative/absolute Anzahl
-                        </h4>
-                        <p class="category">Gibt das Aufkommen der Items an.</p>
-                    </md-card-header>
-                    <md-card-content>
-                        <piechart></piechart>
-                    </md-card-content>
-                </md-card>
-            </div>
-            -->
         </div>
     </div>
 </template>
@@ -215,35 +51,40 @@ import PieChart from "../components/diagrams/PieChart";
 import test from "../components/diagrams/ZoomableBarChart";
 
 import { globalStore } from "../main";
+import HeatMap from "@/components/diagrams/HeatMap";
 
 export default {
     components: {
-        chart: Chart,
-        column: ColumnChart,
-        linechart: LineChart
-        //piechart: PieChart
+        //chart: Chart,
+        heatmap: HeatMap,
+        //column: ColumnChart,
+        //linechart: LineChart
+        piechart: PieChart
     },
     created() {
         this.fetchData();
     },
     data() {
         return {
-            loadedData: {
-                
-            }
+            loadedData: {},
+            selectedDistrict: Object
         };
     },
     methods: {
         getURL: function(url) {
             return globalStore.prefix + url;
         },
-        fetchData(){
+        fetchData() {
             let dashboard = this;
-            d3.json(dashboard.getURL("json/CountsAll.json")).then(function(data) {
+            d3.json(this.getURL("json/CountsAll.json")).then(function(data) {
                 dashboard.loadedData = data;
-                dashboard.$emit("dataLoaded")
-                console.log(dashboard.loadedData["weekday"])
+                dashboard.$emit("dataLoaded");
+                console.log("Dashboard event: CountsAll finished loading");
             });
+        },
+        changeSelectedDistrict(selectedDistrict) {
+            console.log("dashboard data update");
+            console.log(selectedDistrict);
         }
     }
 };
