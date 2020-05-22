671696afaf8ef0c0a3ff1b9df6d225b1ec8d20fe
buchtaro <robin.buchta@stud.hs-hannover.de>
2020-05-22 10:41:22 +0200

    push

--- statistics_bigdata.py
+++ statistics_bigdata.py
@@ -24,11 +24,31 @@ def statistics(df):
     resultObject = {}
 
     for column in df.columns:
-        print("Generate counts of column:", column)
         columnset = df[column]
         resultObject[column] = columnset.value_counts().to_dict()
     
     print("Safe as json")
     with open('website/src/assets/json/CountsAll.json', "w") as outfile: 
+        outfile.write(json.dumps(resultObject))
+
+#Zweiter Lauf für Dashboard
+    df["LocationDescription"] = df["Location Description"]
+    df["PrimaryType"] = df["Primary Type"]
+    df = df.drop(["Location Description"], axis=1)
+    df = df.drop(["Primary Type"], axis=1)
+    
+    resultObject = {}
+    
+    
+    for district in df['District'].unique():
+        districtDf = df.loc[df['District'] == district]
+        districtDf = districtDf.drop(['District'], axis=1)
+        tmp= {}
+        for column in districtDf.columns:
+            columnset = districtDf[column]
+            tmp[column] = columnset.value_counts().to_dict()
+        resultObject[district] = tmp
+            
+    with open('website/src/assets/json/dashboard/CountsPerDistrikt.json', "w") as outfile: 
         outfile.write(json.dumps(resultObject))
 
