dc69ba4d4cbb93c6c8b94b4b1ebf08eb0bfa1805
buchtaro <robin.buchta@stud.hs-hannover.de>
2020-06-01 18:42:59 +0200

    Push

--- website/src/routes/routes.js
+++ website/src/routes/routes.js
@@ -1,8 +1,7 @@
 import DashboardLayout from "@/pages/Layout/DashboardLayout.vue";
 
 import Dashboard from "@/pages/Dashboard.vue";
-import Itemsets from "@/pages/FrequentItemsets.vue";
-import AssoTable from "@/pages/AssociationRulesTable.vue";
+
 import Graph from "@/pages/NetworkGraph.vue";
 import Parcoords from "@/pages/ParcoordsTable.vue";
 import Information from "@/pages/Information.vue";
