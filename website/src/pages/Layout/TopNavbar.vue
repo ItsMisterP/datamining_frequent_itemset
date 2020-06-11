832a71ee9ea18d1d02343d48512cd7d1640c0169
Philip <philip.ohm@live.de>
2020-06-11 11:20:48 +0200

    mobile fixed

--- website/src/pages/Layout/TopNavbar.vue
+++ website/src/pages/Layout/TopNavbar.vue
@@ -1,5 +1,22 @@
 <template>
-  <div></div>
+  <md-toolbar md-elevation="0" class="md-transparent">
+    <div class="md-toolbar-row">
+      <div class="md-toolbar-section-start">
+      </div>
+      <div class="md-toolbar-section-end">
+        <md-button
+          class="md-just-icon md-simple md-toolbar-toggle"
+          :class="{ toggled: $sidebar.showSidebar }"
+          @click="toggleSidebar"
+        >
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </md-button>
+
+      </div>
+    </div>
+    </md-toolbar>
 </template>
 
 <script>
@@ -8,14 +25,6 @@ export default {
     return {
       selectedEmployee: null,
       employees: [
-        "Jim Halpert",
-        "Dwight Schrute",
-        "Michael Scott",
-        "Pam Beesly",
-        "Angela Martin",
-        "Kelly Kapoor",
-        "Ryan Howard",
-        "Kevin Malone"
       ]
     };
   },
